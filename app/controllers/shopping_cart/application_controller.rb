6382fbf3dee9f3f0a23312e91782840f0d94a86c
therealloker <therealloker@gmail.com>
2018-09-30 16:14:35 +0300

    loyout application

--- app/controllers/shopping_cart/application_controller.rb
+++ app/controllers/shopping_cart/application_controller.rb
@@ -2,5 +2,6 @@ module ShoppingCart
   class ApplicationController < ::ApplicationController
     include ShoppingCart::CurrentSession
     helper_method :current_order
+    layout 'layouts/application'
   end
 end
